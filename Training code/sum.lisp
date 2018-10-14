47fec9a5292531dfd7bd309b66741eb9af2dd84d
Ryan <ryanbar1997@gmail.com>
2018-10-14 01:42:15 +0100

    Cleaned some stuff.

--- Training code/sum.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage sum-package
-	(:use :common-lisp :common-lisp-user)
-	(:export :sum ))
-
-(in-package sum-package)
-
-(defun sum (x y)
-	(+ x y))
--- unit-test.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage :unit-test
-	(:use :lisp-unit :cl-user :sum-package))
-
-(in-package :unit-test)
-
-(define-test test-sum
-	(assert-equal 5 (sum 3 2))
-	(assert-equal 10 (sum 9 1)))
