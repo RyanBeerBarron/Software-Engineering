51b379a2e7479e7be28fc0ee19f9cfcca4587718
Ryan <ryanbar1997@gmail.com>
2018-10-30 13:14:19 +0000

    Got all of the nil cases to work

--- src/software-engineering.lisp
+++ src/software-engineering.lisp
@@ -1,12 +1,12 @@
 (in-package :software-engineering)
 
 (defun LCA (digraph vertex1 vertex2)
+	(if (eq digraph nil) (return-from LCA nil) ())
 	(if (or (digraph:emptyp digraph)
 			(not (digraph:contains-vertex-p digraph vertex1))
 			(not (digraph:contains-vertex-p digraph vertex2))
 			(eq vertex1 nil)
-			(eq vertex2 nil)
-			(eq digraph nil))
+			(eq vertex2 nil))
 		nil
 		()
 		))
